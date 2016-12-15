ad0560de9cb2528d34b841c2fc8bdd4d2acfc585
Vladislav L <abbizzz@yahoo.com>
2016-12-15 13:58:39 +0100

    logo + multiplicateur

--- js.js
+++ js.js
@@ -1,13 +1,28 @@
 var i = 1
 var clicks = 0;
+var granny = 1;
+
+
    function scoreadd() {
-       clicks += 1;
+       clicks += i;
        document.getElementById("clicks").innerHTML = clicks;
 
    };
 
-   var granny = 0;
+
    	function grannyClick(){
-   		clicks = clicks + 4;
    		document.getElementById("clicks").innerHTML = clicks;
+   		if (clicks >= 2) {
+   			i = i += 2;
+   			clicks -= 2;
+   		}
+   		else{
+   			alert("No more points to buy...");
+   		}
    	};
+
+	function res() {
+			    var tb = document.getElementById('clicks');
+			    tb.value = '';
+
+	}
