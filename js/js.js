1b174f9063d27489dbbc594c6a91b895cf9ccc5d
Vladislav L <abbizzz@yahoo.com>
2016-12-15 15:04:15 +0100

    multi

--- js/js.js
+++ js/js.js
@@ -1,4 +1,5 @@
 var clicks = 0;
+var i = 1;
       function reset(){
 
 clicks = 0;
@@ -6,18 +7,26 @@ document.getElementById("clicks").innerHTML = clicks;
    }
 
    function scoreadd() {
-       clicks += 1;
+       clicks += i;
        document.getElementById("clicks").innerHTML = clicks;
    }
 
    var granny = 0;
    	function grannyClick(){
-   		clicks += 1;
    		document.getElementById("clicks").innerHTML = clicks;
+      if (clicks > 2) {
+        i = i + 1;
+        clicks -= 15;
+      }
+      else{
+        alert("No more points to buy...");
+      }
    	}
 
    function reset(){
 
 clicks = 0;
+i = 1;
+granny = 0
 document.getElementById("clicks").innerHTML = clicks;
    }
