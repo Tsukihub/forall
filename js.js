3df47878848f960db4bf01da6c9efd54ec5d9cd7
Vladislav L <abbizzz@yahoo.com>
2016-12-15 12:03:41 +0100

    multiplicateur granny +4

--- js.js
+++ js.js
@@ -1,11 +1,13 @@
+var i = 1
 var clicks = 0;
    function scoreadd() {
        clicks += 1;
        document.getElementById("clicks").innerHTML = clicks;
+
    };
 
    var granny = 0;
    	function grannyClick(){
-   		clicks += 1;
+   		clicks = clicks + 4;
    		document.getElementById("clicks").innerHTML = clicks;
    	};
